5787a5875da3e4e1780baca2deeed1da217a8e94
code-pankaj <pankajyadavpy069@gmail.com>
2025-10-16 16:40:05 +0530

    escrow program

--- week1/anchor-escrow-starter-q4-25/programs/anchor-escrow-q4-25/src/lib.rs
+++ week1/anchor-escrow-starter-q4-25/programs/anchor-escrow-q4-25/src/lib.rs
@@ -6,7 +6,7 @@ pub mod state;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("5UFZzEt5vU9fxtUAgsD11z63ApZEHJ5bH7Z4QpFwZ2CQ");
+declare_id!("7kJZf74odXcFPoSjyCWieRzasf5S4HPLBXowepNowg19");
 
 #[program]
 pub mod anchor_escrow_q4_25 {
@@ -20,8 +20,8 @@ pub mod anchor_escrow_q4_25 {
         ctx.accounts.refund_and_close_vault()
     }
 
-    // pub fn take(ctx: Context<Take>) -> Result<()> {
-    //     ctx.accounts.deposit()?;
-    //     ctx.accounts.withdraw_and_close_vault()
-    // }
+     pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+     }
 }
